8b6cc434b5d21234cb286c42260b25afb5d0aa10
Parthasarathy <parthaintodesk@gmail.com>
2019-07-14 17:12:20 +0530

    J

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // JWT Auth
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
